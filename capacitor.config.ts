5b2604dffd9623e61100244d7edb2f88f4479a0e
Salim Bakri <saleem.bakri94@gmail.com>
2021-12-19 22:22:54 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Capacitor PoC',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
