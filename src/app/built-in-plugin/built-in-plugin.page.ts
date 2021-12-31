059dd3eaf5e533fb99f18469b9856fb83a5ffbb7
Salim Bakri <saleem.bakri94@gmail.com>
2021-12-30 23:03:06 -0500

    integrate buit-in capacitor plugin

--- src/app/built-in-plugin/built-in-plugin.page.ts
+++ src/app/built-in-plugin/built-in-plugin.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-built-in-plugin',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class BuiltInPluginPage {
 
-  constructor() {}
+  private alertMessage: string;
+
+  constructor(
+    public alertController: AlertController
+  ) {}
+
+  async getlocationMessage(): Promise<string> {
+    return Geolocation.getCurrentPosition()
+    .then(result => `Your longitude is ${result.coords.longitude} and your latitude is ${result.coords.latitude}`)
+    .catch(error => error?.message);
+  };
+
+  async showCurrentPosition() {
+    this.alertMessage = await this.getlocationMessage();
+    let alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Location',
+      subHeader: 'Current Location is:',
+      message: this.alertMessage,
+      buttons: ['OK']
+    });
+    await alert.present();
+    alert.onDidDismiss().then(() => alert = null);
+  }
 
 }
