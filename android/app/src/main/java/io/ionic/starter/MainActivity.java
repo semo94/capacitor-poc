ef2db421879ba5f8a783529e3e620db73c95d178
Salim Bakri <saleem.bakri94@gmail.com>
2022-01-04 14:28:31 -0500

    integrate custom plugin with ionic poc app

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,23 @@
 package io.ionic.starter;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+//import ContactsPlugin;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+//    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+//      // Additional plugins you've installed go here
+//      add(ContactsPlugin.class);
+//    }});
+  }
+
+}
--- src/app/custom-plugin/custom-plugin.page.ts
+++ src/app/custom-plugin/custom-plugin.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContactsPlugin } from 'poc-contacts-plugin/src';
 
 @Component({
   selector: 'app-custom-plugin',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class CustomPluginPage {
 
+  contacts = [];
   constructor() {}
+
+  async loadContacts() {
+    this.contacts = (await ContactsPlugin.getContacts('')).results;
+    console.log('my contacts: ', this.contacts);
+  }
 
 }
